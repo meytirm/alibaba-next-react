ded5d076a55e77f0446cc6278b6eb8acd2ce0187
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-06-12 18:12:19 +0330

    Add layouts and pages

---
+++ src/app/[...blog]/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function BlogLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <section>
+        blog is here
+      {children}
+    </section>
+    
+  );
+}
